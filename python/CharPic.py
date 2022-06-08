de981b1063fde91b2f7b602da443f8c4ff115fc8
ZhonghuaYi <yizhonghua6@gmail.com>
2022-06-08 22:24:05 +0800

    file updated. At 10:23PM, 6/8/2022.

--- python/CharPic.py
+++ python/CharPic.py
@@ -4,47 +4,60 @@
 
 import cv2 as cv
 import numpy as np
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 
 class Pic:
     """
     Base class of image.
     """
-    def __init__(self, image_path):
+    def __init__(self, image):
         self.binary_th = None
-        self.edge = None
+        self.image = None
+        self.gray = None
         self.binary = None
-        self.image = cv.imread(image_path, 1)
-        self.gray = cv.imread(image_path, 0)
+        self.edge = None
+        if isinstance(image, str):
+            self.image = cv.imread(image, 1)
+            self.gray = cv.imread(image, 0)
+        elif isinstance(image, np.ndarray):
+            if image.ndim == 3:
+                self.image = image
+                self.gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+            if image.ndim == 2:
+                self.gray = image
+
+    def __getattribute__(self, item):
+        item_value = super().__getattribute__(item)
+        try:
+            if item_value is None:
+                raise TypeError(f"Object attribute '{item}' of class "
+                                f"'{self.__class__.__name__}' is None")
+        except Exception as e:
+            print(e)
+            return None
+        return item_value
 
     def show(self, image_type="image", window_name="Pic", delay=0):
         """
         Show image of given type.
 
         Args:
-            image_type (string, optional): "binary" or "edge" or "gray" or"image". Defaults to "image".
-            window_name (string, optional): Name of displayed window. Defaults to "Pic".
+            image_type (str, optional): "binary" or "edge" or "gray" or"image". Defaults to "image".
+            window_name (str, optional): Name of displayed window. Defaults to "Pic".
             delay (int, optional): Time(ms) that window delay. Defaults to 0.
         """
-        try:
-            if image_type == "binary":
-                if self.binary is None:
-                    raise TypeError("Object attribute 'binary' is None.")
-                image = self.binary
-            elif image_type == "edge":
-                if self.edge is None:
-                    raise TypeError("Object attribute 'edge' is None.")
-                image = self.edge
-            elif image_type == "gray":
-                image = self.gray
-            else:
-                image = self.image
-
-            cv.imshow(window_name, image)
-            cv.waitKey(delay)
-        except TypeError as te:
-            print(te)
+        # if image_type == "binary":
+        #     image = self.binary
+        # elif image_type == "edge":
+        #     image = self.edge
+        # elif image_type == "gray":
+        #     image = self.gray
+        # else:
+        #     image = self.image
+        image = self.__dict__[image_type]
+        cv.imshow(window_name, image)
+        cv.waitKey(delay)
 
     def to_binary(self, method=2, thresh=-1):
         """
@@ -71,8 +84,8 @@ class Pic:
             if method == 2:
                 self.binary_th, self.binary = cv.threshold(
                     self.gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-        except ValueError as ve:
-            print(ve)
+        except Exception as e:
+            print(e)
         return self.binary
 
     def to_edge(self, thresh1, thresh2, use_binary=False):
@@ -87,15 +100,10 @@ class Pic:
         Returns:
             ndarray: Edge image.
         """
-        try:
-            if use_binary:
-                if self.binary is None:
-                    raise TypeError("Object attribute 'binary' is None.")
-                self.edge = cv.Canny(self.binary, thresh1, thresh2)
-            else:
-                self.edge = cv.Canny(self.gray, thresh1, thresh2)
-        except TypeError as te:
-            print(te)
+        if use_binary:
+            self.edge = cv.Canny(self.binary, thresh1, thresh2)
+        else:
+            self.edge = cv.Canny(self.gray, thresh1, thresh2)
         return self.edge
 
     def to_binary_and_edge(self,
@@ -127,8 +135,10 @@ class Pic:
             size (tuple, optional): (width, height) of the new image. Defaults to None.
         """
         if size is not None:
-            self.image = cv.resize(self.image, size)
-            self.gray = cv.resize(self.gray, size)
+            if self.image is not None:
+                self.image = cv.resize(self.image, size)
+            if self.gray is not None:
+                self.gray = cv.resize(self.gray, size)
             if self.binary is not None:
                 self.binary = cv.resize(self.binary, size)
             if self.edge is not None:
@@ -142,25 +152,25 @@ class CharPic(Pic):
     Args:
         Pic : Base class of image.
     """
-    def __init__(self, image_path):
+    def __init__(self, image):
         """
         Initialize CharPic object.
 
         Args:
-            image_path (string): Image path.
+            image_path (str): Image path.
         """
-        super().__init__(image_path)
+        super().__init__(image)
         self.char_matrix = None
         self.char_image = None
         self.__charSet = ".\'`^\",:Il!i><~+_-?]}1)|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkho*#MW&8%B@$"
 
-    def to_char(self, image=None, charset=None):
+    def char_matrix(self, image=None, charset=None):
         """
         Generate char matrix from image.
 
         Args:
             image (ndarray, optional): Where char matrix is from. Defaults to None.
-            charset (string, optional): Char set to represent grayscale from 0 to 255. 
+            charset (str, optional): Char set to represent grayscale from 0 to 255.
                                          Defaults to None.
 
         Returns:
@@ -180,7 +190,7 @@ class CharPic(Pic):
         Generate char matrix from gray image.
 
         Args:
-            charset (string, optional): Char set to represent grayscale from 0 to 255. 
+            charset (str, optional): Char set to represent grayscale from 0 to 255.
                                         Defaults to None.
 
         Returns:
@@ -193,7 +203,7 @@ class CharPic(Pic):
         Generate char matrix from binary image.
 
         Args:
-            charset (string, optional): Char set to represent grayscale from 0 to 255. 
+            charset (strg, optional): Char set to represent grayscale from 0 to 255.
                                         Defaults to None.
 
         Returns:
@@ -206,21 +216,30 @@ class CharPic(Pic):
         Generate char matrix from edge image.
 
         Args:
-            charset (string, optional): Char set to represent grayscale from 0 to 255. 
+            charset (str, optional): Char set to represent grayscale from 0 to 255.
                                         Defaults to None.
 
         Returns:
             ndarray: char matrix.
         """
         return self.to_char(self.edge, charset)
-    
-    def generate_char_image(self):
-        try:
-            if self.char_matrix is None:
-                raise TypeError("Object attribute 'char_matrix' is None.")
-            
-        except TypeError as te:
-            print(te)
+
+    def generate_char_image(self, font_path, font_size, color=(255, 255, 255)):
+        matrix_shape = self.char_matrix.shape
+        font = ImageFont.truetype(font_path, font_size)
+        canvas_size = (matrix_shape[1] * font_size,
+                       matrix_shape[0] * font_size)
+        canvas = Image.new("RGB", canvas_size)
+        canvas_draw = ImageDraw.Draw(canvas)
+        for i in range(matrix_shape[0]):
+            for j in range(matrix_shape[1]):
+                canvas_draw.text((j * font_size, i * font_size),
+                                 self.char_matrix[i][j],
+                                 font=font,
+                                 fill=color)
+        char_image = cv.cvtColor(np.asarray(canvas), cv.COLOR_RGB2BGR)
+        self.char_image = char_image
+        return self.char_image
 
     def save_char_matrix(self, file_path):
         np.savetxt(file_path, self.char_matrix, fmt="%s")
@@ -228,6 +247,7 @@ class CharPic(Pic):
 
 if __name__ == '__main__':
     image = "./test.jpg"
+    font_path = "C:/Windows/Fonts/Monaco.ttf"
     i = Pic(image)
-    i.resize((200, 300))
-    i.to_binary_and_edge(100, 200)
+    i.to_binary_and_edge(20, 80)
+    i.show("edge")
---
+++ python/CharVideo.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+# @Author:  ZhonghuaYi
+# @Time  :  2022-06-08
+
+
+
+if __name__ == '__main__':
+    pass
